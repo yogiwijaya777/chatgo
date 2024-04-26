887f11dcfa5b6dad4cf9cc8d3840b26cec1bcc68
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-04-26 19:35:39 +0700

    remove create room valdiation

--- server/src/validations/room.validation.js
+++ server/src/validations/room.validation.js
@@ -1,14 +1,6 @@
 const Joi = require('joi');
 const { objectId } = require('./custom.validation');
 
-const createRoom = {
-  body: Joi.object().keys({
-    name: Joi.string().required(),
-    userId: Joi.string().custom(objectId).required(),
-    userId2: Joi.string().custom(objectId).required(),
-  }),
-};
-
 const getRoom = {
   params: Joi.object().keys({
     roomId: Joi.string().custom(objectId).required(),
