bb1b23f08bd87c30479abf1bc2c3069d5ed47884
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-04-26 11:38:42 +0700

    CLIENT | COnversations | NoChatSelectefd

--- client/src/components/messages/NoChatSelected.tsx
+++ client/src/components/messages/NoChatSelected.tsx
@@ -1,10 +1,7 @@
-import React from "react";
-
 const NoChatSelected = () => {
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="flex flex-col items-center gap-2 px-4 text-center font-semibold text-black sm:text-lg md:text-xl">
-        <p>Welcome 👋 John Doe </p>
         <p>Select a chat to start messaging</p>
       </div>
     </div>
